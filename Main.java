22fd2110508d745a41ce6fd950f9f05a0e6032d6
RoseS-coding <145237774+RoseS-coding@users.noreply.github.com>
2024-11-03 15:13:10 -0600

    Update Main.java
    Added in execute corresponding role

--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
-package logic;
+package userLogic;
 
 import java.util.Scanner;
+import roleActions.*;
 
 public class Main {
 	public static void main(String args[]) {
 		UserService userService = new UserService();
 		Scanner scanner = new Scanner(System.in);
 		
+		//TOOD reformat inputs to be app based
+		
+		
+		//Initial asking for credentials
 		System.out.print("Enter username: ");
 		String username = scanner.nextLine();
 		System.out.print("Enter password: ");
 		String password = scanner.nextLine();
-		
-		
 		User user = logincheck(username, password, userService);
+		
+		//Repeat asking for login info until matching credentials found
 		while (user == null) {
 			System.out.print("Enter username: ");
 			username = scanner.nextLine();
@@ -24,8 +29,17 @@ public class Main {
 			user = logincheck(username, password, userService);
 		}
 		
+		executeUserAction(user);
+		
 		scanner.close();
 	}
+	
+	
+	
+	
+	
+	
+	//method to validate user information
 	public static User logincheck(String username, String password, UserService userService) {
 		User user = userService.validateUser(username, password);
 		if (user != null) {
@@ -35,5 +49,24 @@ public class Main {
 			System.out.println("User not found.");
 			return null;
 		}
+	}
+	
+	public static void executeUserAction(User user) {
+		PersonAction userAction;
+		switch (user.getUserType()) {
+		case ADMIN:
+			userAction = new AdminAction();
+			break;
+		case USER:
+			userAction = new UserAction();
+			break;
+		case DRIVER:
+			userAction = new DriverAction();
+			break;
+		default:
+			throw new IllegalStateException("Unexpected value: " + user.getUserType());
+		}
+		
+		userAction.performAction();
 	}
 }
