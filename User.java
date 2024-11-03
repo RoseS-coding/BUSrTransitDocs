aea71d76a824ec7d74e11f43426cad870bba0b11
RoseS-coding <145237774+RoseS-coding@users.noreply.github.com>
2024-11-03 14:40:09 -0600

    Update User.java

--- User.java
+++ User.java
@@ -2,12 +2,16 @@ package logic;
 
 public class User {
 	private String userID;
+	private String username;
+	private String password; //TODO change this to password hash and calculate password hash for check
 	private String name;
 	private String email;
 	private UserType userType;
 	
-	public User(String i, String name, String email, UserType userType) {
+	public User(String i, String username, String password, String name, String email, UserType userType) {
 		this.userID = i;
+		this.username = username;
+		this.password = password;
 		this.name = name;
 		this.email = email;
 		this.userType = userType;
@@ -15,6 +19,14 @@ public class User {
 	
 	public String getUserID() {
 		return userID;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
 	}
 	
 	public String getName() {
