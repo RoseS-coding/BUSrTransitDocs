b7b8a700080655bc41a8e4434ac48c020cb8d19d
RoseS-coding <145237774+RoseS-coding@users.noreply.github.com>
2024-11-04 16:55:12 -0600

    Update AdminAction.java

--- roleActions/AdminAction.java
+++ roleActions/AdminAction.java
@@ -1,8 +1,33 @@
 package roleActions;
 
+import routeLogic.Route;
+import routeLogic.BusRouteManager;
+import java.util.Scanner;
+
 public class AdminAction implements PersonAction {
+	
+	private BusRouteManager busRouteManager;
+	private Scanner scanner;
+	
+	public AdminAction() {
+		this.busRouteManager = new BusRouteManager();
+		this.scanner = new Scanner(System.in);
+	}
+	
 	@Override
 	public void performAction() {
 		System.out.println("Admin action performed");
 	}
+	
+	//Custom admin screen will be opened if user is admin
+	
+	/** ADMIN actions
+	 *  Create and change routes
+	 *  Create and delete drivers
+	 *  Search all user accounts and create/delete
+	 *  Update maintenance status of buses
+	 *  Manage driver schedules
+	 */
+	
+	
 }
