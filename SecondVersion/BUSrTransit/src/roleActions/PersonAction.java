c48a7e672470a08b648871fff029880ba5531c5c
RoseS-coding <145237774+RoseS-coding@users.noreply.github.com>
2024-12-02 12:12:42 -0600

    Delete SecondVersion/BUSrTransit directory

--- SecondVersion/BUSrTransit/src/roleActions/PersonAction.java
+++
@@ -1,6 +0,0 @@
-package roleActions;
-
-public interface PersonAction {
-	//action for user role type
-	void performAction();
-}
--- SecondVersion/BUSrTransit/src/roleActions/UserAction.java
+++
@@ -1,8 +0,0 @@
-package roleActions;
-
-public class UserAction implements PersonAction{
-	@Override
-	public void performAction() {
-		System.out.println("User action performed");
-	}
-}
